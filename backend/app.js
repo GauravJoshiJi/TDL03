aef7064dec1c828b9cba3df84f92ed7e86075a18
GauravJoshiJi <gauravjoshi1997.joshi@gmail.com>
2023-12-27 16:14:06 +0530

    5 commit: REST full API and CRUD operations

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8000;
 const auth = require("./routes/auth");
 const mongoDB = require("./connection/conn");
-
+const list = require("./routes/list");
 mongoDB();
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -11,6 +11,7 @@ app.get("/", (req, res) => {
 app.use(express.json());
 
 app.use("/api/v1", auth);
+app.use("/api/v2", list);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
